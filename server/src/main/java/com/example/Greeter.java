925a3540ae933baba93b81950208a0f8bcb36393
Maximedubois31 <73292860+Maximedubois31@users.noreply.github.com>
2021-06-18 11:27:35 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -3,12 +3,12 @@ package com.example;
 
 public class Greeter {
 
- /** greeter java doc */
+ /** @greeter java doc. */
   public Greeter() {
 
   }
 
-  /** greeter java doc */
+  /** @greeter java doc */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
